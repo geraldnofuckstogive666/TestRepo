120d97cf4f665a32bc9a6b075bee75ca5dae9ced
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-01-18 16:04:12 +0000

    Changed the logic in the addition function

--- addition.py
+++ addition.py
@@ -1,1 +1,12 @@
-print('Hello WOrld!')
+def main():
+    a = eval(input("Enter first num: "))
+    b = eval(input("Enter second num: "))
+    print(add(a, b))
+
+
+def add(a,b):
+    return a + b
+
+
+if __name__ == '__main__':
+    main()
