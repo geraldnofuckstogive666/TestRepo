56bd490bb9f522782e279120201addc2d5bd5ccb
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-01-18 16:55:16 +0000

    Added Subtraction function

---
+++ subtraction.py
@@ -0,0 +1,12 @@
+def main():
+    a = eval(input("Enter first num: "))
+    b = eval(input("Enter second num: "))
+    print(subtract(a, b))
+
+
+def subtract(a,b):
+    return a - b
+
+
+if __name__ == '__main__':
+    main()
